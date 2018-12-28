8896bf4920045280bb602906d5d8d551ba217e85
shcho <sunghwancho2011@gmail.com>
2018-12-28 15:15:01 +0900

    Update: update script

--- script/run.sh
+++ script/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+docker stop streamsets-kinesis
+docker rm streamsets-kinesis
 
-docker run -t --rm -d \
+docker run -t -d \
  -v $STREAMSETS_DATA:/data -p 18630:18630 \
  --name streamsets-kinesis streamsets-kinesis:3.6.1

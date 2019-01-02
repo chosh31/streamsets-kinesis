5e59cdf885136bba3c8a0dcc62bd5616389b9745
shcho <sunghwancho2011@gmail.com>
2019-01-02 16:39:04 +0900

    Build: mount file system data folder

--- script/run.sh
+++ script/run.sh
@@ -3,5 +3,7 @@ docker stop streamsets-kinesis
 docker rm streamsets-kinesis
 
 docker run -t -d \
- -v $STREAMSETS_DATA:/data -p 18630:18630 \
+ -v $STREAMSETS_DATA:/data \
+ -v $STREAMSETS_FS_DATA:/fsData \
+ -p 18630:18630 \
  --name streamsets-kinesis streamsets-kinesis:3.6.1
